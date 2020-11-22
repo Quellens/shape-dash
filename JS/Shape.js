9bfe1bf40e09b4e37df3551609c391456bb63e74
Quellens <joelseckershady@gmail.com>
2020-11-22 19:06:00 +0100

    :sparkles:

--- JS/Shape.js
+++ JS/Shape.js
@@ -12,14 +12,12 @@ export default class Shape {
             circle.style.borderRadius = "50%";
             circle.style.width = "20vmin";
             circle.style.height = "20vmin";
-            document.getElementById("game").appendChild(circle);
             this.element = circle;
         } else if(this.form == "rectangle"){
             var rectangle = document.createElement("button");
             rectangle.style.backgroundColor = this.color;
             rectangle.style.width = "25vmin";
             rectangle.style.height = "12vmin";
-            document.getElementById("game").appendChild(rectangle);
             this.element = rectangle;
         } else if(this.form == "triangle"){
             const possibilities = ["Right","Top","Left","Bottom"];
@@ -27,21 +25,22 @@ export default class Shape {
             var triangle = document.createElement("div");
             triangle.classList.add(direction);
             triangle.style["border" + direction]  = "15vmin solid " + this.color;
-            document.getElementById("game").appendChild(triangle);
             this.element = triangle;
         } else if(this.form == "square"){
             var square = document.createElement("button");
             square.style.backgroundColor = this.color;
             square.style.width = "20vmin";
             square.style.height = "20vmin";
-            document.getElementById("game").appendChild(square);
             this.element = square;
         }
-
 
         this.element.style.position = "absolute";
         this.element.style.left = this.position.x;
         this.element.style.top = this.position.y;
+    }
 
+    remove(){
+        this.element.remove();
+        console.log(this);
     }
 }
--- JS/index.js
+++ JS/index.js
@@ -5,17 +5,35 @@ game.style.backgroundColor = "grey";
 
 
 var shapes = [ "square", "circle", "rectangle", "triangle"];
-var colors = ["red", "green"];
-const shape = new Shape({
+var colors = ["red", "green", "#6E44FF","#BA274A", "#2191FB", "#EF7A85","#4A6C6F","#C0BCB5"];
+let shape = new Shape({
     form: shapes[Math.floor(Math.random()*shapes.length)],
     color: colors[Math.floor(Math.random()*colors.length)],
     position: {
         x: randomIntFromRange(game.clientWidth * 0.2, game.clientWidth * 0.8) + "px",
         y: randomIntFromRange(game.clientHeight * 0.2, game.clientHeight * 0.8) +"px",
     }
-}
-    );
+});
+
 shape.create();
+game.appendChild(shape.element);
+shape.element.addEventListener("click", ()=>{
+    shape.element = undefined;
+    
+    shape = new Shape({
+        form: shapes[Math.floor(Math.random()*shapes.length)],
+        color: colors[Math.floor(Math.random()*colors.length)],
+        position: {
+            x: randomIntFromRange(0, game.clientWidth * 0.8) + "px",
+            y: randomIntFromRange(0, game.clientHeight * 0.8) +"px",
+        }
+    });
+    console.log(shape);
+    setTimeout(()=>{
+        shape.create();
+        game.appendChild(shape.element);
+    }, Math.random()* 3000);
+})
 
 console.log(shape);
 
